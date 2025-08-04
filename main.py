8b9de807cb1cbae55d27f86ae7ed0b8449661995
JAY <muleshraddha154@gmail.com>
2025-08-04 09:48:34 +0530

    commited to dev branch

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def print_hi(name):
     print("india is my country")
     print("shraddha")
     print("bs batch")
+    print("this is my dev branch")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
